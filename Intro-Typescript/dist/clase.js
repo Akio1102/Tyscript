394618adeb15b38c25d85fe1984decddaa8e5aca
Akio1102 <cristiandaviddiazaza@gmail.com>
2023-08-22 08:10:02 -0500

    feat (Intro-Typescript): ✨ Creacion Interface
    Creacion de la Interface para el generico del response de la API

--- Intro-Typescript/dist/clase.js
+++ Intro-Typescript/dist/clase.js
@@ -1,15 +1,42 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
 class Astronauta {
     /*     public id:number;
       public nombre:string;
       public record:string; */
-    constructor(id, nombre, _record) {
+    constructor(id, nombre, _record, mision = []) {
         this.id = id;
         this.nombre = nombre;
         this._record = _record;
+        this.mision = mision;
         /*     this.id = id,
         this.nombre = nombre,
         this.record = record */
+    }
+    getlauches() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const url = `https://api.spacexdata.com/v3/launches`;
+            const { data } = yield axios_1.default.get(url);
+            data.forEach((element, index) => {
+                /*       console.log(index, element.mission_name);*/
+                this.mision[index + 1] = element.mission_name;
+            });
+            console.log(this.mision);
+            return this.mision;
+        });
     }
     set record(record) {
         this._record = record;
@@ -25,6 +52,37 @@ class Astronauta {
 }
 const astro1 = new Astronauta(1, "Julian", "techo");
 astro1.record = "Artemis";
+astro1.getlauches();
 console.log(astro1);
 console.log(`Record ${astro1.record}`);
 console.log(astro1.recompensa(astro1.record));
+class Detalles {
+    constructor(nombre, descripcion, fechacreacion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.fechacreacion = fechacreacion;
+    }
+    get year() {
+        return this.fechacreacion.getFullYear();
+    }
+}
+class Cohete extends Detalles {
+    constructor(orbita, nombre, descripcion, fechacreacion) {
+        super(nombre, descripcion, fechacreacion);
+        this.orbita = orbita;
+    }
+}
+let falcon1 = new Cohete("luz", "falcon1", "Super Cohete de Elon", new Date());
+console.log(falcon1.year);
+class Tiposcohetes extends Detalles {
+    constructor(nombre, descripcion, fechacreacion) {
+        super(nombre, descripcion, fechacreacion);
+        this.cohetes = [];
+    }
+    agregarCohete(cohete) {
+        this.cohetes.push(cohete);
+    }
+}
+let tiposcohetes = new Tiposcohetes("falcon", "Falcon de SpaceX", new Date());
+tiposcohetes.agregarCohete(falcon1);
+console.log(falcon1, tiposcohetes);
--- Intro-Typescript/src/clase.ts
+++ Intro-Typescript/src/clase.ts
@@ -1,3 +1,6 @@
+import axios from "axios";
+import { APIResponse } from "./interfaces/api.Interface";
+
 class Astronauta {
   /*     public id:number;
     public nombre:string;
@@ -5,11 +8,24 @@ class Astronauta {
   constructor(
     public readonly id: number,
     public nombre: string,
-    private _record: string
+    private _record: string,
+    public mision: string[] = []
   ) {
     /*     this.id = id,
     this.nombre = nombre,
     this.record = record */
+  }
+
+  async getlauches() {
+    const url = `https://api.spacexdata.com/v3/launches`;
+    const { data } = await axios.get<APIResponse[]>(url);
+
+    data.forEach((element: any, index: number) => {
+      /*       console.log(index, element.mission_name);*/
+      this.mision[index + 1] = element.mission_name;
+    });
+    console.log(this.mision);
+    return this.mision;
   }
 
   public set record(record: string) {
@@ -30,7 +46,51 @@ class Astronauta {
 const astro1 = new Astronauta(1, "Julian", "techo");
 
 astro1.record = "Artemis";
+astro1.getlauches();
 
 console.log(astro1);
 console.log(`Record ${astro1.record}`);
 console.log(astro1.recompensa(astro1.record));
+
+class Detalles {
+  constructor(
+    public nombre: string,
+    public descripcion: string,
+    public fechacreacion: Date
+  ) {}
+
+  get year() {
+    return this.fechacreacion.getFullYear();
+  }
+}
+
+class Cohete extends Detalles {
+  constructor(
+    public orbita: string,
+    nombre: string,
+    descripcion: string,
+    fechacreacion: Date
+  ) {
+    super(nombre, descripcion, fechacreacion);
+  }
+}
+
+let falcon1 = new Cohete("luz", "falcon1", "Super Cohete de Elon", new Date());
+
+console.log(falcon1.year);
+
+class Tiposcohetes extends Detalles {
+  public cohetes: Cohete[] = [];
+  constructor(nombre: string, descripcion: string, fechacreacion: Date) {
+    super(nombre, descripcion, fechacreacion);
+  }
+  agregarCohete(cohete: Cohete) {
+    this.cohetes.push(cohete);
+  }
+}
+
+let tiposcohetes = new Tiposcohetes("falcon", "Falcon de SpaceX", new Date());
+
+tiposcohetes.agregarCohete(falcon1);
+
+console.log(falcon1, tiposcohetes);
